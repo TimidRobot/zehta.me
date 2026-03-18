b73ac11a7e8d6e21ae738170b78b05a73eeffc36
Timid Robot Zehta <timid.robot@zehta.me>
2026-03-18 08:27:01 +0100

    update for best practices indicated by ruff

--- source/packages/custom_jinja2_filters/lektor_custom_jinja2_filters.py
+++ source/packages/custom_jinja2_filters/lektor_custom_jinja2_filters.py
@@ -1,8 +1,11 @@
-# -*- coding: utf-8 -*-
-# Standard library
+"""
+Custom Jinja2 filters for Lektor.
+"""
+
+# Standard library imports
 import re
 
-# Third-party
+# Third party imports
 from dateutil import parser
 from lektor.pluginsystem import Plugin
 
@@ -10,28 +13,35 @@ RE_DATE_PREFIX = re.compile(r"^(\d{8}-)(.*)$")
 
 
 class CustomJinja2FiltersPlugin(Plugin):
+    """
+    Custom Jinja2 filters for Lektor.
+    """
+
     name = "custom-jinja2-filters"
     description = "Custom Jinja2 filters"
 
-    def on_setup_env(self, **extra):
+    def on_setup_env(self, **extras):  # noqa: ARG002 Unused method argument
+        """
+        Hook into the `setup-env` Lektor event.
+        """
+
         def parsedatetime_filter(value):
             """
-            Convert string into a datetime.datetime object using
-            dateutil.parser
+            Convert string into a datetime.datetime object.
+
+            Uses dateutil.parser.
             """
-            datetime_obj = parser.parse(value)
-            return datetime_obj
+            return parser.parse(value)
 
         self.env.jinja_env.filters["parsedatetime"] = parsedatetime_filter
 
         def stripdateprefix_filter(value):
             """
-            Strip date prefix using a regular expression
+            Strip date prefix using a regular expression.
             """
             matches = RE_DATE_PREFIX.match(value)
             if matches:
                 return matches[2]
-            else:
-                return value
+            return value
 
         self.env.jinja_env.filters["stripdateprefix"] = stripdateprefix_filter
--- source/packages/custom_jinja2_filters/setup.py
+++ source/packages/custom_jinja2_filters/setup.py
@@ -1,17 +1,25 @@
-# Standard library
-import ast
-import io
-import re
+"""
+Setup lektor_custom_jinja2_filters.
 
+Setup lektor_custom_jinja2_filters, a collection of custom Jinja2 filters for
+Lektor.
+"""
+
+# Standard library
+# Standard library imports
+import ast
+import re
+from pathlib import Path
+
+# Third party imports
 # Third-party
 from setuptools import find_packages, setup
 
-with io.open("README.md", "rt", encoding="utf8") as f:
+with Path("README.md").open("rt", encoding="utf8") as f:
     readme = f.read()
 
 _description_re = re.compile(r"description\s+=\s+(?P<description>.*)")
-
-with open("lektor_custom_jinja2_filters.py", "rb") as f:
+with Path("lektor_custom_jinja2_filters.py").open("rb") as f:
     description = str(
         ast.literal_eval(
             _description_re.search(f.read().decode("utf-8")).group(1)
@@ -20,11 +28,8 @@ with open("lektor_custom_jinja2_filters.py", "rb") as f:
 
 setup(
     author="Timid Robot Zehta",
-    author_email="timid.robot@zehta.me",
     description=description,
-    install_requires=[
-        "python-dateutil",
-    ],
+    install_requires=["python-dateutil"],
     keywords="Lektor plugin",
     license="CC0-1.0",
     long_description=readme,
@@ -32,7 +37,8 @@ setup(
     name="lektor-custom-jinja2-filters",
     packages=find_packages(),
     py_modules=["lektor_custom_jinja2_filters"],
-    # url='[link to your repository]',
+    url="https://github.com/TimidRobot/zehta.me/tree/main/source/packages"
+    "/custom-jinja2-filters",
     version="0.3",
     classifiers=[
         "Environment :: Plugins",
@@ -43,7 +49,7 @@ setup(
     entry_points={
         "lektor.plugins": [
             "custom-jinja2-filters ="
-            " lektor_custom_jinja2_filters:CustomJinja2FiltersPlugin",
+            " lektor_custom_jinja2_filters:CustomJinja2FiltersPlugin"
         ]
     },
 )
