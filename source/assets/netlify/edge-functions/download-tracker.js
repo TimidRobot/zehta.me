af61d81740b41b78a81e4f829561cfd8f5145aad
Timid Robot Zehta <timid.robot@zehta.me>
2026-06-01 07:15:47 +0200

    add download tracker

---
+++ source/assets/netlify/edge-functions/download-tracker.js
@@ -0,0 +1,23 @@
+// https://docs.netlify.com/build/edge-functions/overview/
+// https://plausible.io/docs/events-api
+import type { Context } from "@netlify/edge-functions";
+export default async (request: Request, context: Context) => {
+  const fileUrl = new URL(request.url);
+  fetch("https://zehta.me/418/api/event", {
+    method: "POST",
+    body: JSON.stringify({
+      "domain": "zehta.me"
+      "name": "Download Tracker",
+      "url": URL(request.url)
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      "User-Agent": request.headers.get("User-Agent"),
+      "X-Forwarded-For": request.headers.get("X-Forwarded-For")
+    }
+  })
+    .then((response) => console.log(response));
+  //console.log("LOGMESSAGE");
+  return;
+}
+// vim: set ts=2 sw=2 et:
