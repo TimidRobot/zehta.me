3a2e8222fd2b8877b439a7a4757b48a673daa0e6
Timid Robot Zehta <timid.robot@zehta.me>
2026-06-01 09:08:15 +0200

    publish

--- docs/netlify/edge-functions/download-tracker.js
+++ docs/netlify/edge-functions/download-tracker.js
@@ -1,1 +1,5 @@
-export default async(request)=>{console.log("download-tracker 1");fetch("https://zehta.me/418/api/event",{method:"POST",body:JSON.stringify({"domain":"zehta.me","name":"Download Tracker","url":request.url}),headers:{"Content-Type":"application/json","User-Agent":request.headers.get("User-Agent"),"X-Forwarded-For":request.headers.get("X-Forwarded-For")}}).then((response)=>console.log(response));console.log("download-tracker 2");return;}
+export default async(request)=>{console.log("download-tracker begin");console.log(request.headers.get("User-Agent"))
+console.log(request.headers.get("X-Forwarded-For"))
+try{const response=await fetch("https://zehta.me/418/api/event",{method:"POST",body:JSON.stringify({"domain":"zehta.me","name":"Download Tracker","url":request.url}),headers:{"Content-Type":"application/json","User-Agent":request.headers.get("User-Agent"),"X-Forwarded-For":request.headers.get("X-Forwarded-For")}});if(!response.ok){throw new Error(`Response status: ${response.status}`);}
+const result=await response.text();console.log(`download-tracker text: ${result}`);}catch(error){console.error(error.message);}
+console.log("download-tracker return");return;}
